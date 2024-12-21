480346470bd259ec623982fab9da5366f27194fc
SpikeAI23 <officialspikeai@gmail.com>
2024-12-21 14:24:41 +0000

    Version 002

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,16 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
 module.exports = {
-    name: 'ping',
-    description: 'Replies with Pong!',
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    const exampleEmbed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setTitle('Pong!')
+      .setDescription('This is a description of the command.')
+      .setImage('https://i.imgur.com/your-animated-banner-url.gif'); // Replace with your image URL
+
+    await interaction.reply({ embeds: [exampleEmbed] });
+  },
 };
